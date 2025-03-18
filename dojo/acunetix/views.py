2c92115e3e9a2ddf82a7681de155461bc112e238
glebfedorov911 <fedorov22134@gmail.com>
2025-03-18 20:46:15 +0000

    add endpints get with html-template

--- dojo/acunetix/views.py
+++ dojo/acunetix/views.py
@@ -8,7 +8,16 @@ from django.shortcuts import render
 @check_acunetix_api
 def create_exclude_hour_form(request):
     if request.method == "POST":
-        ...
+        main_form = ExcludeHoursForm(request.POST)
+        exclude_hours_form_set = ExcludeHoursFormSet(request.POST)
+
+        if main_form.is_valid() and exclude_hours_form_set.is_valid():
+            print(main_form.cleaned_data)
+            print(exclude_hours_form_set.cleaned_data)
+            print(main_form.cleaned_data.get("name"))
+            print(exclude_hours_form_set.cleaned_data[0].get("day_of_week"))
+
+    
     elif request.method == "GET":
         main_form = ExcludeHoursForm()
         exclude_hours_form_set = ExcludeHoursFormSet()
